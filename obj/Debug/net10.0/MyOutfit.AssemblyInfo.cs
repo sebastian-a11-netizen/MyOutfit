cc8bd3acd86c7c6f805940a16ae24d2a79afd0a7
sebastian-a11-netizen <jsebastianr527@gmail.com>
2026-03-03 20:43:49 -0500

    chat y mejoras

--- obj/Debug/net10.0/MyOutfit.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyOutfit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyOutfit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de892fbfa4b64b26093e8c1289c02e9690d618f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b3666a77b936527386c22ff42f47519eacc3eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyOutfit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyOutfit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
